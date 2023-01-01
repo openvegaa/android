16dad0446a5102f19706d151b3184822a404f97e
Arpit Patidar <arpit.ptdr@hotmail.com>
2023-01-01 20:37:13 +0530

    feat: added alias setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.tsx'],
+        alias: [
+          {
+            src: './src',
+            components: './src/components',
+            screens: './src/screens',
+          },
+        ],
+      },
+    ],
+  ],
 };
